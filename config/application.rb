d3596b0485bb7811d326e93c145fc751cd1172d0
cauecjm <cauecjm@gmail.com>
2018-05-21 13:41:41 -0300

    Desativando configs de db

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require_relative 'boot'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
+require 'action_cable/engine'
+require 'active_job/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
